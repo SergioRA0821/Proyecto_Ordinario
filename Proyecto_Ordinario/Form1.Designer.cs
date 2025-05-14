16a6b79e7cd7661e8273fd41153a64c6c888023a
Lab2-28 <Lab2-28@LAB2-28>
2025-05-13 18:41:08 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto_Ordinario/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Proyecto_Ordinario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbUsuario = new System.Windows.Forms.TextBox();
+            this.txbContra = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(341, 265);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(117, 40);
+            this.btnIngresar.TabIndex = 1;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(337, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(337, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Contraseña";
+            // 
+            // txbUsuario
+            // 
+            this.txbUsuario.Location = new System.Drawing.Point(340, 126);
+            this.txbUsuario.Name = "txbUsuario";
+            this.txbUsuario.Size = new System.Drawing.Size(118, 20);
+            this.txbUsuario.TabIndex = 4;
+            // 
+            // txbContra
+            // 
+            this.txbContra.Location = new System.Drawing.Point(340, 214);
+            this.txbContra.Name = "txbContra";
+            this.txbContra.Size = new System.Drawing.Size(118, 20);
+            this.txbContra.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txbContra);
+            this.Controls.Add(this.txbUsuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnIngresar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbUsuario;
+        private System.Windows.Forms.TextBox txbContra;
+    }
+}
+
---
+++ Proyecto_Ordinario/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Ordinario
+{
+    internal class Usuario
+    {
+        private static List<Usuario> usuarios = new List<Usuario>
+        {
+            new Usuario{NombreUsuario = "Sergio", Contrasenia = "0000"},
+            new Usuario{NombreUsuario = "Sofi", Contrasenia = "1111"}
+        };
+
+        public Usuario() { }
+
+        public List<Usuario> ObtenerUsuarios()
+        {
+            return usuarios;
+        }
+
+        public string NombreUsuario { get; set; }
+        public string Contrasenia { get; set; }
+    }
+}
